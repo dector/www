c0da93b4c4af0ca27b109e0450187455c242ccd6
DBot <codebot@dector.space>
2026-03-15 20:36:05 +0000

    feat(seo): add shared meta/og/twitter support in layouts
    Co-authored-by: D <code@dector.space>

--- src/lib/globals.js
+++ src/lib/globals.js
@@ -1,5 +1,7 @@
 export const G = {
   BlogTitle: "Pragmatic Craftsmanship",
+  BlogDescription:
+    "dector's personal space: thoughts about technologies, coffee and universe",
   PhantomTags: ["code", "coding"],
   CommitUrlTemplate: "https://github.com/dector/www/commit/{{COMMIT}}",
 };
